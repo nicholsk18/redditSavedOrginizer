b1b9d0c27f6d91f0c9297217b96a8f613d133f2d
Karson <nicholsk18@gmail.com>
2021-12-21 07:25:57 -0600

    temp removed api, changing auth and added build for css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./components/Card";
 
 const App = () => {
-  return (
+  const [redditCode, setRedditCode] = useState("")
+  const [redditState, setRedditState] = useState("")
+
+  useEffect(() => {
+    console.log(window.sessionStorage.getItem("redditCode"))
+    console.log(window.sessionStorage.getItem("redditState"))
+  })
+
+  return true ? (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-4 gap-4">
         <Card />
       </div>
     </div>
+  ) : (
+    <a href="https://www.reddit.com/api/v1/authorize?client_id=uNKMxYX6AkglZi36qk3ZFQ&response_type=code&state=abc&redirect_uri=http://localhost:3001/&duration=temporary&scope=*">
+      Test
+    </a>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use("/api", api);
+// app.use("/api", api);
 
 app.use(express.static(path.resolve(__dirname, "../dist")));
 
